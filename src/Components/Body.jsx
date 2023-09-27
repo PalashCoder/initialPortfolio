a6e15c873c4da67f3d803f5b01dac4c98dfe12ac
Palash Das <70379839+iamrealpalash@users.noreply.github.com>
2023-09-27 15:27:42 +0530

    Update Body.jsx

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import Typed from 'react-typed'
+import { Typewriter } from 'react-simple-typewriter'
+
 
 function Body() {
-    return (
-        <>
-            <div className={`bg-[#2699fb] w-full pt-[100px] md:h-[87vh] pb-[200px]`}>
-                <div className='max-w-[1240px] md:mt-20 my-2 mx-auto text-center'>
-                    <div className='md:text-3xl text-xl font-semibold'>
-                        Hey,
-                    </div>
-                    <div className='text-white font-bold text-[35px] sm:text-[50px] md:text-[80px]'>
-                        It's Palash
-                    </div>
-                    <div className='md:text-4xl sm:text-2xl text-l font-mono mt-5'>
-                        <Typed
-                            strings={['A Fullstack Developer and a Programmer']}
-                            typeSpeed={100}
-                            loop={true}
-                            backSpeed={50}
-                        />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className={`bg-[#2699fb] w-full pt-[100px] pb-[150px]`}>
+        <div className='max-w-[1240px] md:mt-20 my-2 mx-auto text-center'>
+          <div className='md:text-3xl text-xl font-semibold'>
+            Hey,
+          </div>
+          <div className='text-white font-bold text-[35px] sm:text-[50px] md:text-[80px]'>
+            It's Palash
+          </div>
+          <div className='md:text-4xl sm:text-2xl text-l font-mono mt-5'>
+            <Typewriter
+              words={['A Fullstack Developer & a Programmer.']}
+              loop={true}
+              cursor
+              cursorStyle='_'
+              typeSpeed={100}
+              deleteSpeed={70}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Body
